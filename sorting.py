6b231fd17b9f803ac7c734fee86b5b9ba802c332
Roberto Herman <roberto.herman021@gmail.com>
2020-07-21 13:57:27 -0700

    finished. not as scary as though

--- sorting.py
+++ sorting.py
@@ -13,9 +13,60 @@ def bubble_sort(arr):
 arr = [10,1,3,4,5,2,6,7,9,8]
 arr2 = [10,9,8,7,6,5,4,3,2,1]
 arr3 = [10,1,2,3,4,5,6,7,8,9]
+arr4 = [1]
+arr5 = []
 # print(bubble_sort(arr))
 # print(bubble_sort(arr2))
 # print(bubble_sort(arr3))
+# print(bubble_sort(arr4))
+# print(bubble_sort(arr5))
+
+# Not as good in Space as other algorithms, but it is consistent. Meaning worse and best cases are the same Time.
+# This is the best way to sort Linked Lists
+# O(n log n) Time | O(n) Space
+def merge_sort(arr):
+    # while array has more than one element, we need to bisect it
+    if len(arr) > 1:
+        # save left and right bisections separately
+        # note that each of these bisections will already be sorted
+        left_arr = merge_sort(arr[:len(arr) // 2])
+        right_arr = merge_sort((arr[len(arr) // 2 : len(arr)]))
+        left_index = 0
+        right_index = 0
+        new_arr = []
+        # when we insert an element from left bisection, we'll move the left index up. same logic for right side
+        # we are comparing the smallest elements in each array, and moving up the index accordingly to check the
+        # second smallest, then third smallest, and so on
+        while left_index < len(left_arr) and right_index < len(right_arr):
+            if left_arr[left_index] < right_arr[right_index]:
+                new_arr.append(left_arr[left_index])
+                left_index += 1
+            else:
+                new_arr.append(right_arr[right_index])
+                right_index += 1
+        # Once one of the indices goes out of bounds, we know we've "emptied" one array, so we simply extend the other
+        if left_index >= len(left_arr):
+            new_arr.extend(right_arr[right_index:])
+        else:
+            new_arr.extend(left_arr[left_index:])
+        # return the sorted bisection
+        return new_arr
+    # otherwise, return the array of length 1
+    else:
+        return arr
+
+print(merge_sort(arr))
+print(merge_sort(arr2))
+print(merge_sort(arr3))
+print(merge_sort(arr4))
+print(merge_sort(arr5))
+
+
+
+
+
+
+
 
 
 # We can make a binary search tree and add a list of numbers to it in O(n) time
